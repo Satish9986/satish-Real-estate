f94bb89020110fdd1338899bd9168f5aa1c3917d
Satish9986 <78915884+Satish9986@users.noreply.github.com>
2021-09-07 17:14:50 +0530

    Add files via upload

---
+++ real estate/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+    Return (
+        <>
+        <h1>welcome About Page</h1>
+        </>
+    );
+};
+
+export default About;
---
+++ real estate/App.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+import Home from "./Home"
+import Home from "./Home"
+import Home from "./Home"
+import { switch, Route, Switch, Router, } from "react-router-dom";
